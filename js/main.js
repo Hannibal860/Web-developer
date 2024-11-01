1a9ba3c9713c0f5636dde0546795c412e04c0095
Hannibal860 <skorokhodeugene@gmail.com>
2024-11-01 13:24:32 -0600

    add project

---
+++ js/main.js
@@ -0,0 +1,62 @@
+
+$('.burger-button, .menu__link').on('click', function(){
+  $('.menu__list').toggleClass('active');
+  $('.burger-button').toggleClass('active');
+  $('body').toggleClass('lock');
+});
+
+$(function () {
+  var mixer = mixitup('.portfolio__content')
+});
+
+$(document).ready(function () {
+  $(".menu__list",).on("click", "a", function (event) {
+    //отменяем стандартную обработку нажатия по ссылке
+    event.preventDefault();  
+
+       var id = $(this).attr('href'),
+
+      top = $(id).offset().top;
+
+    $('body,html').animate({ scrollTop: top }, 1500);
+  });
+});
+
+$(document).ready(function () {
+  $(".header__order").on("click", function (event) {
+    event.preventDefault();
+
+    var id = $(this).attr('href'),
+
+      top = $(id).offset().top;
+
+    $('body,html').animate({ scrollTop: top }, 1500);
+  });
+});
+
+$(document).ready(function () {
+  $(".logo").on("click", function (event) {
+    event.preventDefault();
+
+    var id = $(this).attr('href'),
+
+      top = $(id).offset().top;
+
+    $('body,html').animate({ scrollTop: top }, 1500);
+  });
+});
+
+const menu = document.getElementById("menu")
+
+window.addEventListener("scroll", function() {
+  const scrollPos = window.scrollY
+
+  if (scrollPos>50) {
+    menu.classList.add("sticky")
+
+  } else {
+    menu.classList.remove("sticky")
+  }
+   
+})
+
